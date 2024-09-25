a47ebc59c3b3c1c6906740b5f5d5f5872049213e
hase744 <jugglego744@gmail.com>
2024-09-25 23:40:39 +0900

    abondended webpacker

--- app/javascript/packs/shared/save_page_image.js
+++
@@ -1,9 +0,0 @@
-//window.onload = function() {
-//    console.log("ページを保存")
-//    html2canvas(document.querySelector("body")).then(canvas => { 
-//        let downloadEle = document.createElement("a");
-//        downloadEle.href = canvas.toDataURL("image/png");
-//        downloadEle.download = "canvas.png";
-//        downloadEle.click();
-//    });
-//}
--- app/javascript/packs/user/services/service_cell.js
+++
@@ -1,6 +0,0 @@
-var service_condition_area = document.getElementsByClassName("service_condition_area")
-var service_cell_zone = document.getElementsByClassName("service_cell_zone<%service.id%>")[0]
-//console.log(service_cell_zone.clientWidth*2)
-//service_cell_zone.style.height = service_cell_zone.clientWidth*2 + "px"
-//console.log(service_cell_zone.style.height)
-//console.log(service_condition_area[0].clientHeight)
