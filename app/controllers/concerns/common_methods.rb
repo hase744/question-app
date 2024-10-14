ec60ca823ef487046866c5d64d3cbe488b3533a9
hase744 <jugglego744@gmail.com>
2024-10-14 23:28:43 +0900

    updated label

--- app/controllers/concerns/common_methods.rb
+++ app/controllers/concerns/common_methods.rb
@@ -6,24 +6,6 @@ module CommonMethods
     NKF.nkf('-w -Z4', str)
   end
 
-  
-  def translate_list(word)
-    {
-      "video" => "動画",
-      "image"=> "画像",
-      "text" => "文章",
-      "advisement"=>"相談",
-      "consult"=>"相談",
-      "career"=>"キャリア",
-      "business"=>"ビジネス",
-      "paid"=>"完了",
-      "pending"=>"準備中",
-      "in_transit"=>"送金中",
-      "canceled"=>"キャンセル",
-      "failed"=>"失敗",
-    }[word.to_s]
-  end
-
   def current_nav_item
     @current_nav_item
   end
--- app/models/payout.rb
+++ app/models/payout.rb
@@ -1,4 +1,19 @@
 class Payout < ApplicationRecord
   belongs_to :user
   enum status_name: { paid: 0, pending: 1, in_transit: 2, canceled: 3, failed: 4 }
+
+  def status_japanese
+    case self.status_name
+    when "paid"
+      "完了"
+    when "pending"
+      "準備中"
+    when "in_transit"
+      "送金中"
+    when "canceled"
+      "キャンセル"
+    when "failed"
+      "失敗"
+    end
+  end
 end
