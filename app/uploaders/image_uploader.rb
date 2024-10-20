7020c4c6e032c567453e397009da462f23f8f98d
hase744 <jugglego744@gmail.com>
2024-10-20 22:06:15 +0900

    converted png to jpeg

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,10 +20,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fit: [300, 300]
     process convert: 'jpg'
+
+    def full_filename(for_file)
+      super(for_file).chomp(File.extname(for_file)) + '.jpg'
+    end
   end
 
   version :normal_size do
     process resize_to_fit: [1000, 1000]
+    process convert: 'jpg'
+
+    def full_filename(for_file)
+      super(for_file).chomp(File.extname(for_file)) + '.jpg'
+    end
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
