c3c18b5023af6f43d9949420340a7bae5549e970
hase744 <jugglego744@gmail.com>
2025-01-06 06:03:45 +0900

    upgraded webpack

--- app/javascript/channels/file_channel.js
+++ app/javascript/channels/file_channel.js
@@ -2,7 +2,7 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("FileChannel", {
   connected() {
-    console.log("connected")
+    console.log("connected！")
     // Called when the subscription is ready for use on the server
   },
 
@@ -12,12 +12,18 @@ consumer.subscriptions.create("FileChannel", {
 
   received(data) {
     console.log('received!')
-    console.log(`.${data.img_mapping}`);
+    const firstMapping = data.img_mapping[0];
+    const normalFileUrl = firstMapping.img_url;
     data.img_mapping.forEach(img => {
       console.log(img)
       var imgElements = document.querySelectorAll(`.${img.style_class}`);
       imgElements.forEach(element => {
         element.src = img.img_url;
+        // 親が <a> タグの場合、href を最初のマッピングの URL に更新
+        var parentAnchor = element.closest('a');
+        if (parentAnchor) {
+          parentAnchor.href = normalFileUrl;
+        }
       });
     })
     if(data.img_mapping.length > 0){
